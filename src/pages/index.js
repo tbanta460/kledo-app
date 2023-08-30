1adb53c1b11fcb5d1ee7afa155c0f46c26636fb3
albaprogrammer9@gmail.com <bantakarollah@gmail.com>
2023-08-30 23:10:24 +0700

    index redirect to login

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom"
+
+const SingleIndex = () => {
+    return <Navigate to="/login" />
+}
+
+export default SingleIndex
--- src/routers/index.js
+++ src/routers/index.js
@@ -4,12 +4,18 @@ import {
     RouterProvider,
   } from "react-router-dom";
 import Login from "../pages/login";
+import SingleIndex from "../pages";
 import Profile from "../pages/profile";
 import Dashboard from "../pages/dashboard";
 import ShippingCompany from "../pages/sippingCompany";
 
 const Routers = () => {
     const router = createBrowserRouter([
+        {
+          path:"/",
+          element: <SingleIndex/>
+        },
+
         {
           path: "/login",
           element: <Login />,
